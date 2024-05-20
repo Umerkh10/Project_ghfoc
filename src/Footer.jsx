81f94b0e022a85ebaa84bd720f26d0d2e93091b5
umer <mumarkhan383@gmail.com>
2024-05-20 17:00:21 +0500

    cdn changes

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -114,19 +114,6 @@ function Footer() {
                                     ))}
                                 </div>
 
-                                <div class="social mb-4">
-                                    <h4 class="title mt-4 mb-3">Find Us On</h4>
-                                    <ul className="icon-footer" >
-                                        <li>
-                                            <a href="https://www.facebook.com/">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" /></svg>
-                                            </a>
-                                            <a href="https://pk.linkedin.com/">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" /><rect width="4" height="12" x="2" y="9" /><circle cx="4" cy="4" r="2" /></svg>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
 
                             </div>
                         </div>
